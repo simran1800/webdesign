727bb2c79be71fb086d4a5961b81e391122b33cd
simran1800 <63819877+simran1800@users.noreply.github.com>
2020-06-18 16:02:13 +0530

    Add files via upload

---
+++ dinasaur game/script.js
@@ -0,0 +1,10 @@
+var character=document.getElementById("character");
+var block=document.getElementById("block");
+
+function jump()
+{
+	character.classList.add("animate");
+	setTimeout(function(){
+		character.classList.remove  ("animate");
+	},200);
+}
